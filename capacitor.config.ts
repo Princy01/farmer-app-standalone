9b3882567be0eedf9a9ff65b38a7115e522cff7e
Dheeraj <dheerajraina55@gmail.com>
2025-02-03 20:59:20 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'farmer-app-s',
+  webDir: 'www'
+};
+
+export default config;
